dc64f78e281fde1a13ed581a97beb294410266a4
Qu1zz-hub <77579263+Qu1zz-hub@users.noreply.github.com>
2021-06-02 11:50:52 +0400

    Add files via upload

---
+++ АБОБАиАБЧИХБА/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace АБОБАиАБЧИХБА
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (log.Text.Length > 0)
+            {
+                if (pass.Password.Length > 0)
+                {
+                    mainavik win1 = new mainavik();
+                    win1.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Введите пароль!", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин!", "Ошибка входа", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            sas a1 = new sas();
+            a1.Show();
+            this.Close();
+        }
+    }
+}
---
+++ АБОБАиАБЧИХБА/mainavik.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace АБОБАиАБЧИХБА
+{
+    /// <summary>
+    /// Логика взаимодействия для mainavik.xaml
+    /// </summary>
+    public partial class mainavik : Window
+    {
+        public mainavik()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow nani = new MainWindow();
+            nani.Show();
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            АБОБАиАБЧИХБА.больницаDataSet1 больницаDataSet1 = ((АБОБАиАБЧИХБА.больницаDataSet1)(this.FindResource("больницаDataSet1")));
+            // Загрузить данные в таблицу таблица_о_пациентах. Можно изменить этот код как требуется.
+            АБОБАиАБЧИХБА.больницаDataSet1TableAdapters.таблица_о_пациентахTableAdapter больницаDataSet1таблица_о_пациентахTableAdapter = new АБОБАиАБЧИХБА.больницаDataSet1TableAdapters.таблица_о_пациентахTableAdapter();
+            больницаDataSet1таблица_о_пациентахTableAdapter.Fill(больницаDataSet1.таблица_о_пациентах);
+            System.Windows.Data.CollectionViewSource таблица_о_пациентахViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("таблица_о_пациентахViewSource")));
+            таблица_о_пациентахViewSource.View.MoveCurrentToFirst();
+            // Загрузить данные в таблицу таблица__о_сотрудниках. Можно изменить этот код как требуется.
+            АБОБАиАБЧИХБА.больницаDataSet1TableAdapters.таблица__о_сотрудникахTableAdapter больницаDataSet1таблица__о_сотрудникахTableAdapter = new АБОБАиАБЧИХБА.больницаDataSet1TableAdapters.таблица__о_сотрудникахTableAdapter();
+            больницаDataSet1таблица__о_сотрудникахTableAdapter.Fill(больницаDataSet1.таблица__о_сотрудниках);
+            System.Windows.Data.CollectionViewSource таблица__о_сотрудникахViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("таблица__о_сотрудникахViewSource")));
+            таблица__о_сотрудникахViewSource.View.MoveCurrentToFirst();
+        }
+    }
+}
